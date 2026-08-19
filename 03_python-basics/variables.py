5165eb15dec8ff7fc35c455727789974fa843ae7
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-19 08:35:54 +0200

    worked with variables

--- 03_python-basics/variables.py
+++ 03_python-basics/variables.py
@@ -10,5 +10,41 @@ area = width * length
 print(area)
 
 """
-RULES 
+RULES for Variables!
+  When naming variables there are some
+  rules that must be followed:
+- A variable name must start with a 
+letter or the underscore character
+
+- A variable name cannot start with a number
+
+- A variable name can only contain alpha-numeric characters
+and underscore (A-z, 0-9, and _)
+
+- Variable names are case-sensitive
+(age, Age and AGE are three different variables)
 """
+
+examplevar = 'Python'
+print(examplevar)
+
+example_var = 'Python another variable'
+print(example_var)
+
+_example_var = 'Python another variable'
+print(_example_var)
+
+EXAMPLEVAR = 'Python another variable'
+print(EXAMPLEVAR)
+examplevar2 = 'Python another variable'
+print(examplevar2)
+
+# Illegal Variable names
+# 2exampleVar = 'Python'
+# example-var = 'Python'
+# exmaple var = 'Python'
+
+# Python should use snake_case naming
+
+first_name = 'Sascha'
+print(first_name)
