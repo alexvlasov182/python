3548c460a2e2a38649623946f5e133cafb72eeb4
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-19 08:40:36 +0200

    worked with comments

---
+++ 03_python-basics/comments.py
@@ -0,0 +1,15 @@
+"""
+Comments!
+"""
+
+# Prints Hello Python!
+print('Hello Python!')
+print('Hello World!')
+
+"""
+Python is great for:
+Web Application Framework
+Machine Learning
+AI...
+Etc.
+"""
