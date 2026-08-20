1963cd6f043f74865a6bf22074ad16e7d5c48672
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-20 09:45:49 +0200

    worked with strings concatentaion

---
+++ 04_strings-textdata/strings-conncatination.py
@@ -0,0 +1,13 @@
+"""
+String Concatenation
+"""
+first_name = "Sascha"
+last_name = "Vlasov"
+hello_text = "Hello, {} {}".format(first_name, last_name)
+print(hello_text)
+
+second_name = "Erik"
+number_of_shoes = 8
+hello_statement = f'{first_name} has {number_of_shoes} pairs of shoes'
+print(hello_statement)
+
