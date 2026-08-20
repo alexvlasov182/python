778ee30a140c1374322a09aa81bdf3ca23cd124d
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-20 09:50:37 +0200

    worked with useful methods

---
+++ 04_strings-textdata/useful-strings-methods.py
@@ -0,0 +1,13 @@
+"""Useful Strings Methods"""
+
+message = 'Welcome to Python!'
+
+# Methods are a way to separate code
+
+
+print(message.upper())
+print(message.lower())
+print(message.capitalize())
+print(message.count('o'))
+print(message.endswith('!'))
+print(message.isdigit())
