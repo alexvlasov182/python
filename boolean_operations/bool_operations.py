df0428c269e6543a77bad491a07934d5c1d419c4
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-21 14:57:24 +0200

    finished section boolean and operations

---
+++ boolean_operations/bool_operations.py
@@ -0,0 +1,41 @@
+"""
+Booleans are data types that return only:
+True or False
+"""
+
+# Are you human?
+is_human = True
+do_you_love_fashion = False
+donut = 'Donut'
+fav_number = 7
+print(type(donut))
+print(type(fav_number))
+print(type(is_human))
+
+"""
+Comparison Operators
+== Equal
+!= NotEqual
+>= GreaterThan
+<= LessThan
+> GreaterThanOrEqual
+< LessThanOrEqual
+"""
+
+
+print(2 == 3)
+print(2 != 3)
+print(2 < 3)
+print(2 <= 3)
+print(2 >= 3)
+print(2 >= 3)
+
+"""
+Logical Operators
+and
+or
+not
+"""
+print(2 <= 3 and 4 > 1)
+print(4 > 7 or 1 == 1)
+print(not (7 == 7))
