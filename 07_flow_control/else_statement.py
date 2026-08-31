9be135d96e97e2b027a6e093ff4df87f99753bbc
Oleksandr Vlasov <drumlife182@gmail.com>
2026-08-31 17:47:10 +0200

    started working with flow control

---
+++ 07_flow_control/else_statement.py
@@ -0,0 +1,23 @@
+"""
+Else Statement
+if condition:
+    # block of code to be executed if condition is true
+else:
+ # block of code to be executed
+"""
+
+number = 100
+
+if number == 100:
+    print(f"number is equal to {number}")
+else:
+    print(f"number is not equal to 100")
+
+print("will always be executed")
+
+hour = 12
+if hour == 18:
+    message = "Good morning"
+else:
+    message = "Good afternoon"
+print(message)
---
+++ 07_flow_control/if_statements.py
@@ -0,0 +1,13 @@
+"""
+Flow Control: If Statements
+
+if condition:
+    # block of code to be executed if the condition is true
+"""
+
+number = 100
+
+if number == 100:
+    print("Number is 100")
+
+print("end of application")
