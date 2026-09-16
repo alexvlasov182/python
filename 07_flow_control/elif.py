d7f7c19a8c184c95833cb22b784c8219d6b2842b
Oleksandr Vlasov <drumlife182@gmail.com>
2026-09-16 09:25:48 +0200

    working wiht Loops

--- 07_flow_control/elif.py
+++ 07_flow_control/elif.py
@@ -13,10 +13,12 @@ else:
 
 """
 
+if hour < 15:
 HOUR = 15
 
 if HOUR < 15:
     print("Good morning")
+elif hour < 20:
 elif HOUR < 20:
     print("Good afternoon")
 else:
---
+++ 08_loops/for_loops.py
@@ -0,0 +1,6 @@
+"""
+For Loops: Iteration.
+"""
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12]
+for number in numbers_list:
+    print(number)
