c0f7f7365402ffecac85ad9001b82017cb5fb0ac
Oleksandr Vlasov <drumlife182@gmail.com>
2026-09-16 09:36:43 +0200

    worked with while loops

---
+++ 08_loops/while_loops.py
@@ -0,0 +1,19 @@
+"""
+While Loops!
+"""
+
+x = 0
+
+while x < 10:
+    x = x + 1
+    print(x)
+
+
+# Module
+i = 10
+while 10 >= i > 5:
+    if i % 2 != 0:
+        print(f'{i} is not divisible by 2')
+    else:
+        print(f'{i} is divisible by 2')
+    i -=1
